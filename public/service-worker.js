a893276afc2289680f80ae138b8aa5eb2ea1de15
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2018-02-01 11:06:19 +0200

    remove install listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,3 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/teacher.png'
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
 self.addEventListener('push', function(event) {
   var payload = event.data ? event.data.text() : '{}';
   var notification = JSON.parse(payload).notification;
