f3781c13606e3ed3f4373440ccacf79146ac860b
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2018-02-01 11:15:04 +0200

    do not notify if payload does not look like a notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,11 @@ self.addEventListener('push', function(event) {
   try {
     var payload = event.data.text();
     var notification = JSON.parse(payload).notification;
-    event.waitUntil(
-      self.registration.showNotification(notification.title, notification)
-    );
+    if (notification && notification.title) {
+      event.waitUntil(
+        self.registration.showNotification(notification.title, notification)
+      );
+    }
   } catch (error) {
     console.log(error);
   }
